810e76301fd1cf15848b763a327aade9d5a7893d
RachelShrikiMor <rachelmorhin@gmail.com>
2025-07-26 09:48:15 +0300

    update api_requests
    update api_requests
    for get_data_visitor_ip

--- app_requests/api_requests.py
+++ app_requests/api_requests.py
@@ -1,7 +1,20 @@
 import requests
 
-def get_data_visitor_ip():
-    response = requests.get("https://ipapi.co/json/")
-    data = response.json()
-    return data
-    #print(data)
+def get_data_visitor_ip(ip):
+    """
+    this function fey a data of web visitor by ip address
+    return value from function can be None or
+    {'ip': '2a0d:6fc2:4570:8800:7d25:29c8:77d2:7cbe', 'network': '2a0d:6fc2:4540::/42', 'version': 'IPv6', 'city': 'Rishon LeTsiyyon', 'region': 'Central District', 'region_code': 'M', 'country': 'IL', 'country_name': 'Israel', 'country_code': 'IL', 'country_code_iso3': 'ISR', 'country_capital': 'Jerusalem', 'country_tld': '.il', 'continent_code': 'AS', 'in_eu': False, 'postal': None, 'latitude': 31.9642, 'longitude': 34.7876, 'timezone': 'Asia/Jerusalem', 'utc_offset': '+0300', 'country_calling_code': '+972', 'currency': 'ILS', 'currency_name': 'Shekel', 'languages': 'he,ar-IL,en-IL,', 'country_area': 20770.0, 'country_population': 8883800, 'asn': 'AS12400', 'org': 'Partner Communications Ltd.'}
+    """
+    #response = requests.get("https://ipapi.co/json/")
+    response = requests.get(f"https://ipapi.co/{ip}/json/")
+
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            # print(data)
+            return data
+        else:
+            return None
+    else:
+        return None
--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import app_requests.api_requests as api_requests
 import streamlit as st
 
 print("weather app")
-api_requests.get_data_visitor_ip()
+ip = "2a0d:6fc2:4570:8800:7d25:29c8:77d2:7cbe"
+visitor_data =  api_requests.get_data_visitor_ip(ip)
+#print(visitor_data)
+
+# change color
+latitude = visitor_data["latitude"]
+longitude = visitor_data["longitude"]
+country_capital = visitor_data["country_capital"]
+country_name = visitor_data["country_name"]
+city = visitor_data["city"]
+
+
